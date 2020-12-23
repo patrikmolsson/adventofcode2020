268ea39f83dfdf6f7a73010a92ac134b48f2b79d
Patrik Olsson <patrik.m.olsson@gmail.com>
2020-12-23 12:20:00 +0100

    Solve star one as graph as well

--- Day-23/Program.cs
+++ Day-23/Program.cs
@@ -2,58 +2,50 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
-var one = new Action(() =>
+void One()
 {
-    var input = "469217538";
+    var (cups, activeCup) = BuildGraph(0);
 
-    var cups = input.ToCharArray().Select(s => int.Parse(s.ToString())).ToList();
-
-    var activeCup = cups[0];
     var moves = 100;
+    Run(cups, activeCup, moves);
 
-    for (var i = 1; i <= moves; i++)
+    var str = new StringBuilder();
+    var c = cups[1].Next;
+    while (c != cups[1])
     {
-        var activeIndex = cups.IndexOf(activeCup);
-
-        // Figure out which cups
-        var pickedUpCups = cups.Concat(cups).Skip(activeIndex + 1).Take(3).ToArray();
-
-        // Remove cups
-        cups = cups.Except(pickedUpCups).ToList();
-
-        // Where to place it?
-        var destinationCup = activeCup - 1;
-
-        while (!cups.Contains(destinationCup)) destinationCup = destinationCup == 0 ? cups.Max() : destinationCup - 1;
-
-        var indexOfDestinationCup = cups.IndexOf(destinationCup);
-
-        cups.InsertRange(indexOfDestinationCup + 1, pickedUpCups);
-
-        // Calculate new active cup
-        activeIndex = cups.IndexOf(activeCup);
-
-        activeCup = cups.Concat(cups).ElementAt(activeIndex + 1);
+        str.Append(c.Number);
+        c = c.Next;
     }
-
-    var indexOfOne = cups.IndexOf(1);
-
-    var str = string.Join("", cups.Concat(cups).Skip(indexOfOne + 1).TakeWhile(s => s != 1).Select(s => s.ToString()));
-
+    
     Console.WriteLine($"[One] String: {str}");
-});
-var two = new Action(() =>
+}
+
+void Two()
 {
     var stopwatch = Stopwatch.StartNew();
-    var input = "469217538";
 
+    var (cups, activeCup) = BuildGraph(999_991);
+
+    var moves = 10_000_000;
+    Run(cups, activeCup, moves);
+
+    var first = cups[1].Next.Number;
+    var second = cups[1].Next.Next.Number;
+
+    Console.WriteLine($"[Two] First: {first} Second: {second} Product: {(long) first * second}. El: {stopwatch.Elapsed}");
+}
+
+(IDictionary<int, Cup> cups, Cup activeCup) BuildGraph(int additionalCups)
+{
+    var input = "469217538";
     var cups = new Dictionary<int, Cup>();
 
     // Build graph
     Cup lastCup = null;
     var cupNumbers = input.ToCharArray().Select(s => int.Parse(s.ToString()))
-        .Concat(Enumerable.Range(10, 999_991))
+        .Concat(Enumerable.Range(input.Length + 1, additionalCups))
         .ToArray();
 
     foreach (var cupNumber in cupNumbers)
@@ -66,14 +58,17 @@ var two = new Action(() =>
         lastCup = current;
     }
 
-    lastCup.Next = cups[cupNumbers[0]];
+    var activeCup = cups[cupNumbers[0]];
+    lastCup.Next = activeCup;
 
-    var activeCupNumber = cupNumbers[0];
-    var moves = 10_000_000;
+    return (cups, activeCup);
+}
+
+void Run(IDictionary<int, Cup> cups, Cup activeCup, int moves)
+{
     for (var i = 1; i <= moves; i++)
     {
-        var activeCup = cups[activeCupNumber];
-
+        // Get slice
         var startPickedUp = activeCup.Next;
         var endPickedUp = activeCup.Next.Next.Next;
 
@@ -82,28 +77,23 @@ var two = new Action(() =>
         endPickedUp.Next = null;
 
         // Where to place it?
-        var destinationCupNumber = activeCupNumber - 1;
+        var destinationCupNumber = activeCup.Number - 1;
         while (!cups.ContainsKey(destinationCupNumber) || startPickedUp.ChainContains(destinationCupNumber))
             destinationCupNumber = destinationCupNumber == 0 ? cups.Count : destinationCupNumber - 1;
 
         // Insert slice
         var destinationCup = cups[destinationCupNumber];
-        var destNext = destinationCup.Next;
-
+        endPickedUp.Next = destinationCup.Next;
         destinationCup.Next = startPickedUp;
-        endPickedUp.Next = destNext;
 
-        // New active cup number
-        activeCupNumber = activeCup.Next.Number;
+        // New active cup
+        activeCup = activeCup.Next;
     }
+}
 
-    var first = cups[1].Next.Number;
-    var second = cups[1].Next.Next.Number;
+One();
 
-    Console.WriteLine($"[Two] First: {first} Second: {second} Product: {(long) first * second}. El: {stopwatch.Elapsed}");
-});
-one();
-two();
+Two();
 
 internal class Cup
 {
